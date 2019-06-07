ddaa06997417385debf8bcc1309a97fca29e1406
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-07 10:40:02 -0500

    WEBDB-CHALLENGE: +index.js +server.js
    Added index.js and server.js. Yarn start. localhost:8000 successful

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+//const projectRouter = require("./data/routers/projectRouter.js");
+//const actionRouter = require("./data/routers/actionRouter.js");
+
+const server = express();
+server.use(express.json());
+
+//const cors = require("cors");
+//server.use(cors());
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Ready, set go! for Sprint Challenge</h2>`);
+});
+
+//server.use("/api/projects", projectRouter);
+//server.use("/api/actions", actionRouter);
+
+module.exports = server;
